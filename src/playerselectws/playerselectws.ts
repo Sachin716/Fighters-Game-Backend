1da9e54c844b8f8953c8c33a79fbbeee4b7c37b7
sachin716 <indegamer1@gmail.com>
2025-02-01 20:11:50 +0530

    user game joing and connection setup awating hange logic

--- src/playerselectws/playerselectws.ts
+++ src/playerselectws/playerselectws.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server,Socket } from 'socket.io';
+import { v4 as uid } from 'uuid';
 
 WebSocketGateway(3000 ,{
     namespace:'playerselect',
@@ -9,33 +10,33 @@ WebSocketGateway(3000 ,{
 export class Playerselectws implements OnGatewayConnection {
 
     @WebSocketServer() server : Server
-    games = new Map()
+    games:Map<string,{client:string , selectionIndex:number , Selected:boolean}[]> = new Map()
 
-    generateGameId(){
-        const newGameId = Math.floor(Math.random()*100000)
-        return newGameId
+    generateUniqueRoomId(): string {
+        return uid(); // Generate a unique ID for the room (v4 UUID)
     }
 
 
     makeNewGame(){
-        var newGameId = this.generateGameId()
-        this.games.forEach((gameid,users)=>{
-            if(gameid == newGameId){
-
-            }
-        })
+        var newGameId = this.generateUniqueRoomId()
+        this.games.set(newGameId,[])
+        return newGameId
     }
 
     handleConnection(client: Socket) {
         var isUserPlacedInGame = false
         this.games.forEach((gameId,users)=>{
-            if(users.length != 2){
-                users.append({ client:client.id , selectionIndex:13 , Selected:false })
+            if(gameId.length != 2){
+                gameId.push({ client:client.id , selectionIndex:13 , Selected:false })
+                client.join(users)
                 isUserPlacedInGame = true
             }
         })
         if(isUserPlacedInGame){
-
+        }
+        else{
+            const newGame = this.makeNewGame()
+            this.games.get(newGame).push({ client:client.id , selectionIndex:1 , Selected:false })
         }
         
     }
