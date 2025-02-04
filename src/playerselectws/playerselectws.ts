e1c9023a60defe61408625761b20836fabeac8cd
sachin0985 <sachin.sharma2@bharuwasolutions.com>
2025-02-04 16:56:58 +0530

    added ability to host the api on local server

--- src/playerselectws/playerselectws.ts
+++ src/playerselectws/playerselectws.ts
@@ -1,53 +1,72 @@
 import { Injectable } from '@nestjs/common';
 import { MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server,Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { v4 as uid } from 'uuid';
 
-WebSocketGateway(3000 ,{
-    namespace:'playerselect',
-    cors:'*'
+WebSocketGateway({
+    namespace: 'playerselect',
+    cors: '*'
 })
 export class Playerselectws implements OnGatewayConnection {
 
-    @WebSocketServer() server : Server
-    games:Map<string,{client:string , selectionIndex:number , Selected:boolean}[]> = new Map()
+    @WebSocketServer() server: Server
+    games: Map<string, { client: string, selectionIndex: number, Selected: boolean }[]> = new Map()
 
     generateUniqueRoomId(): string {
         return uid(); // Generate a unique ID for the room (v4 UUID)
     }
 
 
-    makeNewGame(){
+    makeNewGame() {
         var newGameId = this.generateUniqueRoomId()
-        this.games.set(newGameId,[])
+        this.games.set(newGameId, [])
         return newGameId
     }
 
     handleConnection(client: Socket) {
         var isUserPlacedInGame = false
-        this.games.forEach((gameId,users)=>{
-            if(gameId.length != 2){
-                gameId.push({ client:client.id , selectionIndex:13 , Selected:false })
+        this.games.forEach((gameId, users) => {
+            if (gameId.length != 2) {
+                gameId.push({ client: client.id, selectionIndex: 13, Selected: false })
                 client.join(users)
                 isUserPlacedInGame = true
+                client.join(users)
+                this.server.emit("Connection", { gameid: users, client: client.id, selectionIndex: 13, Selected: false })
+                this.server.to(users).emit("Game_Joined", { client_id: client.id, message: "Player Selection WS Joined Successfully" })
             }
         })
-        if(isUserPlacedInGame){
+        if (isUserPlacedInGame) {
         }
-        else{
+        else {
             const newGame = this.makeNewGame()
-            this.games.get(newGame).push({ client:client.id , selectionIndex:1 , Selected:false })
+            this.games.get(newGame).push({ client: client.id, selectionIndex: 1, Selected: false })
+            client.join(newGame)
+            this.server.emit("Connection", { gameid: newGame, client: client.id, selectionIndex: 1, Selected: false })
+            this.server.to(newGame).emit("Game_Joined", { client_id: client.id, message: "Player Selection WS Joined Successfully" })
         }
-        
+
+
+
     }
 
-    
 
-    
-    
+
+
+
     @SubscribeMessage('change')
-    handleChange(){
-
+    handleChange(client: Socket, data: { selectionIndex: number, Selected: boolean }) {
+        var FoundGame = null
+        this.games.forEach((games, users) => {
+            if (games[0].client == client.id) {
+                games[0] = { client: client.id, selectionIndex: data.selectionIndex, Selected: data.Selected }
+                FoundGame = users
+            }
+            else if (games[1].client == client.id) {
+                games[1] = { client: client.id, selectionIndex: data.selectionIndex, Selected: data.Selected }
+                FoundGame = users
+            }
+        })
+        this.server.to(FoundGame).emit("changed", {})
     }
 
 
